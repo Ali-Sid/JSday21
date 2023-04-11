8bd7dafe94448be5dc2cc4d34c746f6957beb9c1
Ali-Sid <59683051+Ali-Sid@users.noreply.github.com>
2023-04-11 19:34:19 +0530

    3 files uploaded

---
+++ day21/exercise.js
@@ -0,0 +1,44 @@
+// Day 21: DOM
+// Exercises: Level 1
+
+// 1. Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+// refer index.html for the solution and refer inspect page, console tab
+// This is the first paragraph
+
+// 2. Get each of the the paragraph using document.querySelector('#id') and by their id
+// refer index.html, inspect page -> console tab in the browser for the solution
+
+// 3. Get all the p as nodeList using document.querySelectorAll(tagname) and by their tag name
+// refer index.html, inspect page -> console tab in the browser for the solutions
+
+// 4. Loop through the nodeList and get the text content of each paragraph
+// refer index.html, inspect page -> console tab in the browser for the solution
+
+// 5. Set a text content to paragraph the fourth paragraph,Fourth Paragraph
+// refer the browser for results, index.html for code
+
+// 6. Set id and class attribute for all the paragraphs using different attribute setting methods
+// refer index.html
+
+// Exercises: Level 2
+
+// 1. Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+// refer index.html and local host port:5500
+
+// 2.  Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color.
+// refer html file and local host
+
+// 3. Set text content, id and class to each paragraph
+// refer html and browser
+
+// Exercise: Level 3
+/**
+ * DOM: Mini project 1
+Develop the following application, use the following HTML elements to get started with. You will get the same code on starter folder. Apply all the styles and functionality using JavaScript only.
+
+The year color is changing every 1 second
+The date and time background color is changing every on seconds
+Completed challenge has background green
+Ongoing challenge has background yellow
+Coming challenges have background red
+ */
